0e3391be98a9a954ed4c1acde76ee2d7152605e4
Pedro Martin <pedro.martin@red-badger.com>
2020-02-04 22:30:45 +0000

    Parse CSV into an object

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import csvParse from 'csv-parse';
+
+const airTableStoriesPath = path.resolve(
+  __dirname,
+  '../data/airtable-stories.csv',
+);
+
+const parser = csvParse(
+  { delimiter: ',', bom: true, columns: true },
+  (error, data) => {
+    if (error) {
+      throw Error(`Unable to parse CSV, ${error}.`);
+    }
+    return data;
+  },
+);
+
+const getStories = async () => {
+  const storiesStream = createReadStream(airTableStoriesPath).pipe(parser);
+  const stories: Array<{ [key: string]: string }> = [];
+
+  return new Promise((resolve, reject) => {
+    storiesStream.on('error', error => reject(error));
+    storiesStream.on('data', story => stories.push(story));
+    storiesStream.on('end', () => resolve(stories));
+  });
+};
+
+const start = async () => {
+  const stories = await getStories();
+  console.log({ stories });
+};
+
+start();
